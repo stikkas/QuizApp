7ea065a8c2beb658e1d60448ae67479491d4ce94
Blagodatskih Serge <stikkas17@gmail.com>
2025-02-02 21:29:34 +0300

    start question activity

--- app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/quizapp/QuizQuestionsActivity.kt
@@ -1,20 +1,64 @@
 package com.example.quizapp
 
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.content.ContextCompat
+import com.example.quizapp.data.model.getQuestion
+import com.example.quizapp.data.model.isLastQuestion
+import com.example.quizapp.databinding.ActivityQuizQuestionsBinding
 
 class QuizQuestionsActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityQuizQuestionsBinding.inflate(layoutInflater)
+    }
+    private var currentPosition: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_quiz_questions)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+        setQuestion()
+    }
+
+    private fun setQuestion() {
+        val question = getQuestion(currentPosition)
+        setDefaultOptions()
+        val pos = currentPosition + 1
+        with(binding) {
+            progressBar.progress = pos
+            btnSubmit.text = if (isLastQuestion(currentPosition)) {
+                "FINISH"
+            } else {
+                "SUBMIT"
+            }
+            tvProgress.text = resources.getString(R.string.progress_Text, pos, progressBar.max)
+            tvQuestion.text = question.value
+            ivFlag.setImageResource(question.image)
+            val answers = question.answers
+            tvOptionOne.text = answers[0].value
+            tvOptionTwo.text = answers[1].value
+            tvOptionThree.text = answers[2].value
+            tvOption4.text = answers[3].value
         }
+    }
+
+    private fun setDefaultOptions() {
+        with(binding) {
+            setDefaultOptionView(tvOptionOne)
+            setDefaultOptionView(tvOptionTwo)
+            setDefaultOptionView(tvOptionThree)
+            setDefaultOptionView(tvOption4)
+        }
+    }
+
+    private fun setDefaultOptionView(option: TextView) {
+        option.setTextColor(Color.parseColor("#7A8089"))
+        option.typeface = Typeface.DEFAULT
+        option.background = ContextCompat.getDrawable(
+            this@QuizQuestionsActivity,
+            R.drawable.default_option_border_bg
+        )
     }
 }
